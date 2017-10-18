44cb8c0a52443ad9b895763264660814b60750a3
deividsantos <deividfranciscosantos@hotmail.com>
2017-10-18 13:59:53 -0200

    Segundo Commit

---
+++ src/main/java/Model/Lancamento.java
@@ -0,0 +1,38 @@
+package Model;
+
+public class Lancamento {
+
+    private double valor;
+    private String descricao;
+    private char tipo;
+
+    public Lancamento(double valor, String descricao, char tipo) {
+        this.valor = valor;
+        this.descricao = descricao;
+        this.tipo = tipo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+}
---
+++ src/main/java/Model/Usuario.java
@@ -0,0 +1,18 @@
+package Model;
+
+public class Usuario {
+
+    private String userName;
+
+    public Usuario(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
